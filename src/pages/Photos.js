70ceb21a3dd030e2d022e7cef643ecc31397dc44
Jishnu <jishnu.dev.roy@gmail.com>
2023-01-12 10:07:46 +0530

    refactored

--- src/pages/Photos.js
+++ src/pages/Photos.js
@@ -1,16 +1,15 @@
-import React, {useContext} from "react"
-
+import React, { useContext } from "react"
 import Image from "../components/Image"
-import {Context} from "../Context"
-import {getClass} from "../utils"
+import { Context } from "../Context"
+import { getClass } from "../utils"
 
 function Photos() {
-    const {allPhotos} = useContext(Context)
-    
+    const { allPhotos } = useContext(Context)
+
     const imageElements = allPhotos.map((img, i) => (
         <Image key={img.id} img={img} className={getClass(i)} />
     ))
-    
+
     return (
         <main className="photos">
             {imageElements}
